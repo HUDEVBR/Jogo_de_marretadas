3ae9d9ee53d6dfbc6f039cfa6f941ce0ed63fab5
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2022-01-03 06:15:32 -0300

    commit on Master

--- app.js
+++ app.js
@@ -6,20 +6,28 @@ let pontuacao = document.querySelector('#pontuacao')
 let resultado = 0
 
 function quadradoAleatorio() {
-    quadradoAleatorio.forEach(className => {
-        className.classList.remove('mole')
+    quadrado.forEach(className => {
+        className.classList.remove('topeira')
     })
     let posicaoAleatoria = quadradrado[Math.floor(Math.random() * 9)]
-    posicaoAleatoria.classList.add('mole')
-
-
-    //Verifica a posicao do ID da posicaoAleatoria para a posicaoAcertada
-    //para ser utilizado depois
+    posicaoAleatoria.classList.add('topeira')
+    
+    //Verifica a posicao do ID da posicaoAleatoria para a posicaoAcertada para ser utilizado depois
     posicaoAcertada = posicaoAleatoria.id
-
-
 }
 
 quadrado.forEach(id => {
-    id.addEventListener
+    id.addEventListener('mousecima', () => {
+        if(id.id === posicaoAcertada){
+            resultado = resultado + 1
+            pontuacao.textContent = resultado
+        }
+    })
 })
+
+function moverTopeira(){
+    let tempoId = null
+    tempoId = setInterval(quadradoAleatorio, 1000)
+}
+
+moverTopeira()
